dc62d7f725f763a0678ec8951009415a60b0a8dc
Roh <10230569@id.ohio.gov>
2025-01-22 11:07:43 -0500

    Changed some menuing and information documentation

--- practice console app/src/Menu.java
+++ practice console app/src/Menu.java
@@ -1,5 +1,7 @@
 class Menu {
     public static void displayMainMenu() {
+        System.out.println("Welcome to Paycheck Calculator");
+        System.out.println("By, Andy Roh");
         System.out.println("Main Menu:");
         System.out.println("1. Calculate");
         System.out.println("2. About");
@@ -10,9 +12,11 @@ class Menu {
 
     public static void displayAbout() {
         System.out.println("This is a paycheck calculator app to help you calculate net earnings after deductions.");
+        System.out.println("Disclaimer: This tool is not 100% accurate. It serves to have a general guideline for expectations and future planning.");
+        System.out.println("For best use, please refer to the documentation on GitHub.");
     }
 
     public static void displayHelp() {
-        System.out.println("If you need assistance, check the documentation or contact support.");
+        System.out.println("If you need assistance, check the documentation or contact support at rohjamesandy@gmail.com");
     }
 }
